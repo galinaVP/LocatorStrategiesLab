af7959b8113a1aff99c77b0612a3dc7f6a50365e
galinaVP <galka.prit@gmail.com>
2020-07-13 15:09:08 +0300

    draft1

--- src/main/java/com/mainacad/Lab.java
+++ src/main/java/com/mainacad/Lab.java
@@ -1,47 +1,80 @@
 package com.mainacad;
 
-import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Lab
 {
+    public static final By FIRST_NAME = By.id("firstName");
+    public static final By LAST_NAME = By.id("lastName");
+    public static  final By EMAIL = By.id("userEmail");
+    public static  final By FEMALE = By.xpath("//*[text()='Female']");
+    public static  final By NUMBER = By.id("userNumber");
+    public static  final By DATE_FIELD = By.cssSelector("input#dateOfBirthInput");
+    public static  final By SUBJECT = By.id("subjectsContainer");
+    public static  final By READING = By.xpath("//*[text()='Reading']");
+    public static  final By CHOOSE_FILE = By.cssSelector(".form-control-file");
+    public static  final By ADDRESS = By.id("currentAddress");
+    public static final By SUBMIT = By.tagName("button");
+
+
     public static void main( String[] args )
     {
-        //Создаём системную переменную которая содержит путь к драйверу
-        System.setProperty("webdriver.chrome.driver","/drivers/chromedriverWin.exe");
+        //setupChromeDriverAutomatically
+        WebDriverManager.chromedriver().setup();
 
         //Создаём вебдрайвер
         WebDriver driver = new ChromeDriver();
 
         //Открываем тестовую страницу
-        driver.get("https://www.toolsqa.com/automation-practice-form/");
+        driver.get("https://demoqa.com/automation-practice-form");
 
-        //TODO: Вывести в консоль текст Инфо сообщения
+        WebElement firstName = driver.findElement(FIRST_NAME);
+        firstName.sendKeys("Halyna");
 
-        //TODO: Вывести в консоль текст заголовка формы ("Practice Automation Form")
+        WebElement lastName = driver.findElement(LAST_NAME);
+        lastName.sendKeys("Prytuliak");
 
-        //TODO: Кликнуть линк Partial Link Test
+        WebElement email = driver.findElement(EMAIL);
+        email.sendKeys("galka.prit@gmail.com");
 
-        //TODO: Заполнить поле First name:
+        WebElement female =  driver.findElement(FEMALE);
+        female.click();
 
-        //TODO: Заполнить поле Last name:
+        WebElement number = driver.findElement(NUMBER);
+        number.sendKeys("0976063327");
 
-        //TODO: Выбрать пол
+        WebElement date = driver.findElement(DATE_FIELD);
+        date.click();        
+        WebElement month = driver.findElement(By.cssSelector(".react-datepicker__month-select"));
+        month.sendKeys("September");
+        WebElement year = driver.findElement(By.cssSelector(".react-datepicker__year-select"));
+        year.sendKeys("1991");
+        year.click();
+        WebElement day = driver.findElement(By.cssSelector(".react-datepicker__day--011"));
+        day.click();
 
-        //TODO: Выбрать количество лет опыта
+        WebElement dropDown = driver.findElement(SUBJECT);
+        dropDown.sendKeys("C");
+        //Select subjects = new Select(dropDown);
+        //subjects.selectByIndex(5);
 
-        //TODO: Заполнить поле дата
+        WebElement hobby =  driver.findElement(READING);
+        hobby.click();
 
-        //TODO: Выбрать несколько Automation Tool
+        WebElement chooseFileBtn = driver.findElement(By.xpath("/html//input[@id='uploadPicture']"));
+        chooseFileBtn.click();
 
-        //TODO: Выбрать континент из выпадающего списка
+        WebElement address = driver.findElement(ADDRESS);
+        address.sendKeys("Vinnitsa, Keletska");
 
-        //TODO: Выбрать несколько вариантов из списка Selenium Commands
+        //WebElement submit =  driver.findElement(SUBMIT);
+        //submit.click();
 
-        //TODO: Кликнуть на кнопку Button
-
-        //TODO: Закрыть браузер
-
+        //driver.close();
     }
 }
