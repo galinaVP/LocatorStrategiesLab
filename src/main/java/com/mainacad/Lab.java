721dd053d1bef5d7234553ac99bb650d0671d0f0
galinaVP <galka.prit@gmail.com>
2020-07-13 16:03:22 +0300

    draft2

--- src/main/java/com/mainacad/Lab.java
+++ src/main/java/com/mainacad/Lab.java
@@ -1,6 +1,7 @@
 package com.mainacad;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,11 +16,15 @@ public class Lab
     public static  final By FEMALE = By.xpath("//*[text()='Female']");
     public static  final By NUMBER = By.id("userNumber");
     public static  final By DATE_FIELD = By.cssSelector("input#dateOfBirthInput");
+    public static final By MONTH = By.cssSelector("button#submit");
+    public static final By YAER = By.cssSelector(".react-datepicker__year-select");
+    public static final By DAY = By.cssSelector(".react-datepicker__day--011");
     public static  final By SUBJECT = By.id("subjectsContainer");
     public static  final By READING = By.xpath("//*[text()='Reading']");
     public static  final By CHOOSE_FILE = By.cssSelector(".form-control-file");
     public static  final By ADDRESS = By.id("currentAddress");
-    public static final By SUBMIT = By.tagName("button");
+    public static final By SUBMIT = By.cssSelector("button#submit");
+    public static final By CLOSE = By.id("closeLargeModal");
 
 
     public static void main( String[] args )
@@ -29,9 +34,11 @@ public class Lab
 
         //Создаём вебдрайвер
         WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         //Открываем тестовую страницу
         driver.get("https://demoqa.com/automation-practice-form");
+        driver.manage().window().maximize();
 
         WebElement firstName = driver.findElement(FIRST_NAME);
         firstName.sendKeys("Halyna");
@@ -50,31 +57,36 @@ public class Lab
 
         WebElement date = driver.findElement(DATE_FIELD);
         date.click();        
-        WebElement month = driver.findElement(By.cssSelector(".react-datepicker__month-select"));
+        WebElement month = driver.findElement(MONTH);
         month.sendKeys("September");
-        WebElement year = driver.findElement(By.cssSelector(".react-datepicker__year-select"));
+        WebElement year = driver.findElement(YAER);
         year.sendKeys("1991");
         year.click();
-        WebElement day = driver.findElement(By.cssSelector(".react-datepicker__day--011"));
+        WebElement day = driver.findElement(DAY);
         day.click();
 
         WebElement dropDown = driver.findElement(SUBJECT);
-        dropDown.sendKeys("C");
+        dropDown.click();
+        //dropDown.sendKeys("C");
         //Select subjects = new Select(dropDown);
         //subjects.selectByIndex(5);
 
         WebElement hobby =  driver.findElement(READING);
         hobby.click();
 
-        WebElement chooseFileBtn = driver.findElement(By.xpath("/html//input[@id='uploadPicture']"));
-        chooseFileBtn.click();
+        //WebElement chooseFileBtn = driver.findElement(By.cssSelector("form .form-file input #uploadPicture"));
+        //chooseFileBtn.click();
 
         WebElement address = driver.findElement(ADDRESS);
         address.sendKeys("Vinnitsa, Keletska");
 
-        //WebElement submit =  driver.findElement(SUBMIT);
-        //submit.click();
+        WebElement submit =  driver.findElement(SUBMIT);
+        js.executeScript("arguments[0].scrollIntoView();", submit);
+        submit.click();
 
-        //driver.close();
+        WebElement close = driver.findElement(CLOSE);
+        close.click();
+
+        driver.close();
     }
 }
